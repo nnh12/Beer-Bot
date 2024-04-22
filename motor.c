55ea1b6a22929483613b30791077d212d5512ef5
NPereyM <161257031+NPereyM@users.noreply.github.com>
2024-04-21 22:02:56 -0500

    Update motor.c

--- motor.c
+++ motor.c
@@ -1,3 +1,9 @@
+/*
+   ELEC327 Lab 5 Buzzer Example Code.
+   Drive a piezo buzzer connected between P2.1 and P2.5.
+   Plays a major C chord.
+*/
+
 #include "msp430g2553.h"
 /*
 // Notes for a major C chord
@@ -15,26 +21,48 @@ void main(void){
   BCSCTL1 = CALBC1_1MHZ;    // Set the DCO to 1 MHz
   DCOCTL = CALDCO_1MHZ;     // And load calibration data
 
-  P2DIR |= BIT5 + BIT4; // We need P2.5 to be output
+  P2DIR |= BIT5; // We need P2.5 to be output
   P2DIR |= BIT1; // Set P2.1 as output
-  P2SEL |= BIT5 + BIT4; // P2.5 is TA1.2 PWM output
+  P2SEL |= BIT5; // P2.5 is TA1.2 PWM output
 
-  P1DIR |= BIT5;
-  P1SEL |= BIT5;
-
-  TA0CCR0 = 4000;
-  TA0CCR2 = 3000;
-  TA0CCTL2 = OUTMOD_7;
-  TA0CTL = TASSEL_2 + MC_1; // SMCLK, upmode
+  P1DIR |= BIT5; // We need P2.5 to be output
+  //P1DIR |= BIT1; // Set P2.1 as output
+  P1SEL |= BIT5 ; // P2.5 is TA1.2 PWM output
 
 
-  TA1CCR0 = 4000;
-  TA1CCR2 = 3000;
+
+
+
+  TA1CCR0 = 7000;
+  TA1CCR2 = 5000;
+
   TA1CCTL2 = OUTMOD_7;
+  TA1CCTL1 = OUTMOD_7;
   TA1CTL = TASSEL_2 + MC_1; // SMCLK, upmode
 
   WDTCTL = WDT_ADLY_250;  // Set Watchdog Timer to 1000 ms with VLO
   IE1 |= WDTIE;
   __bis_SR_register(CPUOFF + GIE);
 }
+/*
+#pragma vector=WDT_VECTOR
+__interrupt void watchdog_timer(void)
+{
+  which_period = (which_period + 1) % 4;
+  TA1CCR2 = periods[which_period]>>1;
+  TA1CCR0 = periods[which_period];
+}
+*/
+/*
+#pragma vector = PORT1_VECTOR
+__interrupt void PORT1_ISR(void)
+{
+    if (P1IN & BIT3) //when the button is released
+    {
 
+    }
+
+}
+
+    }
+*/
