9f954765d3d921a1b7874dac7403aa1e7ecab4af
nathan hsiao <nathanhsiao123@gmail.com>
2024-04-24 17:24:11 -0400

    Update ultrasonic_and_motors.c

--- ultrasonic_and_motors.c
+++ ultrasonic_and_motors.c
@@ -55,7 +55,7 @@ void main(void){
   P1OUT = 0x00; //start led off
 
   TA1CCR0 = 7000;
-  TA1CCR2 = 0;
+  TA1CCR2 = 500;
   TA1CCTL2 = OUTMOD_7;
   TA1CTL = TASSEL_2 + MC_1; // SMCLK, upmode
 
@@ -66,17 +66,24 @@ void main(void){
 
   WDTCTL = WDT_ADLY_250;  // Set Watchdog Timer to 1000 ms with VLO
   IE1 |= WDTIE;
+  int count = 0;
 
   while(1){
-      if (P1IN & BIT6){ //checks if button is not pressed
+
+      if (!(P1IN & BIT6)){ //checks if button is not pressed
+          count++;
+      }
+
+      if (count % 2 != 0) {
           P1OUT &= ~BIT0;
           TA1CCR2 = 5000;
           TA0CCR1 = 0;
       }
-      else{
+
+      else if (count % 2 == 0){
           P1OUT = BIT0; //red led on
-          TA0CCR1 = 5000;
           TA1CCR2 = 0;
+          TA0CCR1 = 5000;
       }
 
   }
@@ -106,4 +113,3 @@ __interrupt void PORT1_ISR(void)
 
     }
 */
-
