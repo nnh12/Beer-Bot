230ac7c1fd8f8abbd6d44b22ee533f6dcdd8f009
nathan hsiao <nathanhsiao123@gmail.com>
2024-05-22 12:23:34 -0400

    Add Debug LED for SPI Transmit

--- spi_transmit.c
+++ spi_transmit.c
@@ -1,5 +1,7 @@
 #include <msp430g2553.h>
 
+char text[] = "text\n\r";
+
 void setupSPI() {
     // Configure SPI
     UCB0CTL1 = UCSWRST;                     // Put eUSCI state machine in reset
@@ -16,8 +18,29 @@ void sendSPI(unsigned char data) {
     P2OUT &= ~BIT0;                         // Pull SS low
     while (!(IFG2 & UCB0TXIFG));           // Wait for transmit buffer to be ready
     UCB0TXBUF = data;                       // Send data
-    while (!(IFG2 & UCA0RXIFG));              // Wait for transmission to complete
+    while (!(IFG2 & UCB0RXIFG));              // Wait for transmission to complete
     P2OUT |= BIT0;                          // Pull SS high
+}
+
+void init_uart() {
+    P1SEL |= BIT1 + BIT2;
+
+    P1SEL2 = BIT1|BIT2;
+
+    UCA0CTL1 |= UCSWRST+UCSSEL_2;
+    UCA0BR0 = 52;
+    UCA0BR1 = 0;
+
+    UCA0MCTL = UCBRS_0;
+    UCA0CTL1 &= ~UCSWRST;
+    UC0IE |= UCA0RXIE;  // Enable UART RX interrupt
+}
+
+void serial_output(char *str) {
+    while (*str != 0) {
+        while (!(IFG2 & UCA0TXIFG));
+        UCA0TXBUF = *str++;
+    }
 }
 
 int main(void) {
@@ -26,9 +49,12 @@ int main(void) {
 
     P1DIR |= BIT0;
     P1OUT |= BIT0;
+    init_uart();
 
     while (1) {
         sendSPI(0x01);                      // Send command to turn on LED
+        serial_output(text);
+        P1OUT ^= BIT0;
         __delay_cycles(500000);             // Delay
     }
 }
